be771769e6e16c83cf785d706daacfef9b6827ef
zalmane <oren.elias@gmail.com>
2022-07-28 23:01:57 +0300

    update comments

--- datayoga/blocks/filter/tests/test_filter.py
+++ datayoga/blocks/filter/tests/test_filter.py
@@ -2,24 +2,40 @@ from datayoga.blocks.filter.block import Block
 
 
 def test_filter_sql():
-    block = Block({"language": "sql",
-                   "expression": "age>20"})
+    block = Block(
+        {
+            "language": "sql",
+            "expression": "age>20"
+        }
+    )
     assert block.run([{"fname": "john", "lname": "doe", "age": 25}]) == [{"fname": "john", "lname": "doe", "age": 25}]
 
 
 def test_filter_sql_not():
-    block = Block({"language": "sql",
-                   "expression": "age>20"})
+    block = Block(
+        {
+            "language": "sql",
+            "expression": "age>20"
+        }
+    )
     assert block.run([{"fname": "john", "lname": "doe", "age": 15}]) == []
 
 
 def test_filter_jmespath():
-    block = Block({"language": "jmespath",
-                   "expression": "age>`20`"})
+    block = Block(
+        {
+            "language": "jmespath",
+            "expression": "age>`20`"
+        }
+    )
     assert block.run([{"fname": "john", "lname": "doe", "age": 25}]) == [{"fname": "john", "lname": "doe", "age": 25}]
 
 
 def test_filter_jmespath_not():
-    block = Block({"language": "jmespath",
-                   "expression": "age>`20`"})
+    block = Block(
+        {
+            "language": "jmespath",
+            "expression": "age>`20`"
+        }
+    )
     assert block.run([{"fname": "john", "lname": "doe", "age": 15}]) == []
