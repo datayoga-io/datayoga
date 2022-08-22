aee66cc544ec1b532c2591f16eda0847e80a3247
spicy-sauce <yshirizli@gmail.com>
2022-08-22 11:53:40 +0300

    formatting and removed unused import

--- datayoga/blocks/jmespath_custom_functions.py
+++ datayoga/blocks/jmespath_custom_functions.py
@@ -1,12 +1,9 @@
-import jmespath
 from jmespath import functions
 
-#
-# custom function for Jmespath
 
-
+# custom functions for Jmespath
 class JmespathCustomFunctions(functions.Functions):
 
-    @functions.signature({'types': ['array']})
+    @functions.signature({"types": ["array"]})
     def _func_concat(self, elements):
         return ''.join([str(x) for x in elements])
