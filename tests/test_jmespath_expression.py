493a9981cda76506ba6efda8168509cdc5fcf87f
spicy-sauce <yshirizli@gmail.com>
2022-09-01 09:50:08 +0300

    renamed test

--- tests/test_jmespath_expression.py
+++ tests/test_jmespath_expression.py
@@ -1,7 +1,7 @@
 from datayoga.blocks.expression import JMESPathExpression
 
 
-def test_sql_expression():
+def test_jmespath_expression():
     expression = JMESPathExpression()
     expression.compile("concat([fname,' ',mname,' ',lname])")
     assert expression.search({"fname": "john",  "mname": "george", "lname": "smith"}) == "john george smith"
