7b20c5cca47b84430ce24ca39d8274d868850628
spicy-sauce <yshirizli@gmail.com>
2022-09-01 13:03:52 +0300

    #39 Add upper, lower and capitalize custom functions to JMESPath

--- datayoga/blocks/jmespath_custom_functions.py
+++ datayoga/blocks/jmespath_custom_functions.py
@@ -1,9 +1,23 @@
+import string
+
 from jmespath import functions
 
 
 # custom functions for Jmespath
 class JmespathCustomFunctions(functions.Functions):
 
+    @functions.signature({"types": ["string"]})
+    def _func_capitalize(self, arg):
+        return string.capwords(str(arg))
+
     @functions.signature({"types": ["array"]})
     def _func_concat(self, elements):
         return ''.join([str(x) for x in elements])
+
+    @functions.signature({"types": ["string"]})
+    def _func_lower(self, element):
+        return str(element).lower()
+
+    @functions.signature({"types": ["string"]})
+    def _func_upper(self, element):
+        return str(element).upper()
--- tests/test_jmespath_expression.py
+++ tests/test_jmespath_expression.py
@@ -1,7 +1,26 @@
 from datayoga.blocks.expression import JMESPathExpression
 
+expression = JMESPathExpression()
 
-def test_jmespath_expression():
-    expression = JMESPathExpression()
-    expression.compile("concat([fname,' ',mname,' ',lname])")
-    assert expression.search({"fname": "john",  "mname": "george", "lname": "smith"}) == "john george smith"
+CONCAT_EXPRESSION = "concat([fname,' ',mname,' ',lname])"
+TEST_JSON = {"fname": "JohN",  "mname": "GeorgE", "lname": "SmitH"}
+
+
+def test_jmespath_concat_expression():
+    expression.compile(CONCAT_EXPRESSION)
+    assert expression.search(TEST_JSON) == "JohN GeorgE SmitH"
+
+
+def test_jmespath_capitalize_expression():
+    expression.compile(f"capitalize({CONCAT_EXPRESSION})")
+    assert expression.search(TEST_JSON) == "John George Smith"
+
+
+def test_jmespath_lower_expression():
+    expression.compile(f"lower({CONCAT_EXPRESSION})")
+    assert expression.search(TEST_JSON) == "john george smith"
+
+
+def test_jmespath_upper_expression():
+    expression.compile(f"upper({CONCAT_EXPRESSION})")
+    assert expression.search(TEST_JSON) == "JOHN GEORGE SMITH"
