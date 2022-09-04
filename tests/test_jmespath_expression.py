2b6d3a1e46a8692b6a7e8cadbbdd0c57daad53d6
spicy-sauce <yshirizli@gmail.com>
2022-09-04 15:05:39 +0300

    fixed tests

--- tests/test_jmespath_expression.py
+++ tests/test_jmespath_expression.py
@@ -2,25 +2,22 @@ from datayoga.blocks.expression import JMESPathExpression
 
 expression = JMESPathExpression()
 
-CONCAT_EXPRESSION = "concat([fname,' ',mname,' ',lname])"
-TEST_JSON = {"fname": "JohN",  "mname": "GeorgE", "lname": "SmitH"}
-
 
 def test_jmespath_concat_expression():
-    expression.compile(CONCAT_EXPRESSION)
-    assert expression.search(TEST_JSON) == "JohN GeorgE SmitH"
+    expression.compile("concat([fname,' ',mname,' ',lname])")
+    assert expression.search({"fname": "John",  "mname": "George", "lname": "Smith"}) == "John George Smith"
 
 
 def test_jmespath_capitalize_expression():
-    expression.compile(f"capitalize({CONCAT_EXPRESSION})")
-    assert expression.search(TEST_JSON) == "John George Smith"
+    expression.compile("capitalize(name)")
+    assert expression.search({"name": "john george smith"}) == "John George Smith"
 
 
 def test_jmespath_lower_expression():
-    expression.compile(f"lower({CONCAT_EXPRESSION})")
-    assert expression.search(TEST_JSON) == "john george smith"
+    expression.compile(f"lower(product)")
+    assert expression.search({"product": "PhonE"}) == "phone"
 
 
 def test_jmespath_upper_expression():
-    expression.compile(f"upper({CONCAT_EXPRESSION})")
-    assert expression.search(TEST_JSON) == "JOHN GEORGE SMITH"
+    expression.compile(f"upper(product)")
+    assert expression.search({"product": "PhonE"}) == "PHONE"
