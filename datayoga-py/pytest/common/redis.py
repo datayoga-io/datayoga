808da24947a10c925ad72bbbd2bdc2e1778b9e3b
spicy-sauce <yshirizli@gmail.com>
2022-10-06 10:35:24 +0300

    testcontainers 3.7.0 with redis password support

--- datayoga-py/pytest/common/redis.py
+++ datayoga-py/pytest/common/redis.py
@@ -1,6 +1,5 @@
-from typing import Optional, Union
+from typing import Optional
 
-from testcontainers.core.container import DockerContainer
 from testcontainers.redis import RedisContainer
 
 import redis
@@ -23,10 +22,5 @@ def get_redis_client(host: str, port: int, password: Optional[str] = None) -> Re
         raise ValueError(f"can not connect to Redis on {host}:{port}:\n {e}")
 
 
-def get_redis_oss_container(redis_port: int, redis_password: Optional[str] = None) -> Union[DockerContainer, RedisContainer]:
-    if redis_password:
-        return DockerContainer(image="redis:latest").\
-            with_bind_ports(6379, redis_port).\
-            with_command(f"redis-server --requirepass {redis_password}")
-    else:
-        return RedisContainer().with_bind_ports(6379, redis_port)
+def get_redis_oss_container(redis_port: int, redis_password: Optional[str] = None) -> RedisContainer:
+    return RedisContainer(password=redis_password).with_bind_ports(6379, redis_port)
