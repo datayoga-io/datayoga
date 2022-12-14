b8ffd05f74720351b41c42dd2e5366900cbcbdac
spicy-sauce <yshirizli@gmail.com>
2022-12-14 08:30:40 +0200

    dataclass

--- core/src/datayoga_core/result.py
+++ core/src/datayoga_core/result.py
@@ -1,21 +1,16 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
 
 Status = Enum("Status", "SUCCESS REJECTED FILTERED")
 
 
+@dataclass
 class Result():
-    def __init__(self, status: Status, message: Optional[str] = None):
-        self.status = status
-        self.message = message
-
-    def __eq__(self, other):
-        return (
-            isinstance(other, self.__class__) and
-            self.status == other.status and
-            self.message == other.message)
+    status: Status
+    message: Optional[str] = None
 
     @staticmethod
     def success() -> Result:
@@ -24,3 +19,8 @@ class Result():
     @staticmethod
     def reject(message: Optional[str] = None) -> Result:
         return Result(Status.REJECTED, message)
+
+
+@dataclass
+class SuccessResult:
+    Result.success()
