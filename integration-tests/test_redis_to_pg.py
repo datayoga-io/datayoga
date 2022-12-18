d5f6fb0b4efcd2d0a63490fd90bfa1a64b33f9d6
spicy-sauce <yshirizli@gmail.com>
2022-12-18 17:17:29 +0200

    write.cassandra block

--- integration-tests/test_redis_to_pg.py
+++ integration-tests/test_redis_to_pg.py
@@ -1,4 +1,3 @@
-import json
 import logging
 
 import common.pg as pg
@@ -8,6 +7,7 @@ from common.utils import run_job
 logger = logging.getLogger("dy")
 
 REDIS_PORT = 12554
+SCHEMA = "hr"
 
 
 def test_redis_to_pg():
@@ -15,55 +15,35 @@ def test_redis_to_pg():
     redis_container.start()
 
     redis_client = redis.get_redis_client("localhost", REDIS_PORT)
-    redis_client.xadd(
-        "emp",
-        {"message":
-         json.dumps({"_id": 1, "fname": "john", "lname": "doe", "country_code": 972, "country_name": "israel",
-                     "credit_card": "1234-1234-1234-1234", "gender": "M", "__$$opcode": "d"})})
-
-    redis_client.xadd(
-        "emp",
-        {"message":
-         json.dumps({"_id": 2, "fname": "jane", "lname": "doe", "country_code": 972, "country_name": "israel",
-                     "credit_card": "1000-2000-3000-4000", "gender": "F", "__$$opcode": "u"})})
-
-    redis_client.xadd(
-        "emp",
-        {"message":
-         json.dumps({"_id": 12, "fname": "john", "lname": "doe", "country_code": 972, "country_name": "israel",
-                     "credit_card": "1234-1234-1234-1234", "gender": "M", "__$$opcode": "u"})})
+    redis.add_to_emp_stream(redis_client)
 
     postgres_container = pg.get_postgres_container()
     postgres_container.start()
 
     engine = pg.get_engine(postgres_container)
-    pg.create_emp_table(engine, "hr")
-    engine.execute("INSERT INTO hr.emp (id, full_name, country, gender) VALUES (1, 'John Doe', '972 - ISRAEL', 'M')")
-    engine.execute("INSERT INTO hr.emp (id, full_name, country, gender) VALUES (10, 'john doe', '972 - ISRAEL', 'M')")
-    engine.execute(
-        "INSERT INTO hr.emp (id, full_name, country, gender, address) VALUES (12, 'steve steve', '972 - ISRAEL', 'M', 'main street')")
+    pg.create_emp_table(engine, SCHEMA)
+    pg.insert_to_emp_table(engine, SCHEMA)
 
     run_job("tests.redis_to_pg")
 
     total_employees = pg.select_one_row(engine, "select count(*) as total from hr.emp")
     assert total_employees["total"] == 3
 
-    first_employee = pg.select_one_row(engine, "select * from hr.emp where id = 1")
+    first_employee = pg.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 1")
     assert first_employee is None
 
-    second_employee = pg.select_one_row(engine, "select * from hr.emp where id = 2")
+    second_employee = pg.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 2")
     assert second_employee["id"] == 2
     assert second_employee["full_name"] == "Jane Doe"
     assert second_employee["country"] == "972 - ISRAEL"
     assert second_employee["gender"] == "F"
 
-    second_employee = pg.select_one_row(engine, "select * from hr.emp where id = 12")
+    second_employee = pg.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 12")
     assert second_employee["id"] == 12
     assert second_employee["full_name"] == "John Doe"
     assert second_employee["country"] == "972 - ISRAEL"
     assert second_employee["gender"] == "M"
     assert second_employee["address"] == "main street"
-
 
     redis_container.stop()
     postgres_container.stop()
