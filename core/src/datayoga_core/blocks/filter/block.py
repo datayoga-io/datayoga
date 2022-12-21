af6940c364ea17d212230c298c08f88350c1b197
zalmane <oren.elias@gmail.com>
2022-12-21 20:27:02 +0200

    fix filter

--- core/src/datayoga_core/blocks/filter/block.py
+++ core/src/datayoga_core/blocks/filter/block.py
@@ -4,7 +4,7 @@ from typing import Any, Dict, List, Tuple
 from datayoga_core import expression, utils
 from datayoga_core.block import Block as DyBlock
 from datayoga_core.context import Context
-from datayoga_core.result import Result
+from datayoga_core.result import Result, Status
 
 logger = logging.getLogger("dy")
 
@@ -16,6 +16,11 @@ class Block(DyBlock):
 
     async def run(self, data: List[Dict[str, Any]]) -> Tuple[List[Dict[str, Any]], List[Result]]:
         logger.debug(f"Running {self.get_block_name()}")
+        # all rows are filtered until proven otherwise
+        is_filtered = {row[Block.MSG_ID_FIELD]:True for row in data}
         return_data = self.expression.filter(data)
+        # mark filtered rows
+        is_filtered.update({row[Block.MSG_ID_FIELD]:False for row in return_data})
+        logger.warn([Result(Status.FILTERED) if filtered else Result(Status.SUCCESS) for filtered in is_filtered.values()])
 
-        return utils.all_success(return_data)
+        return return_data,[Result(Status.FILTERED) if filtered else Result(Status.SUCCESS) for filtered in is_filtered.values()]
