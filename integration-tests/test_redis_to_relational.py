b3fcf1a2c0fd69ae2d17d763df08c8e4bec97925
vd2org <33198864+vd2org@users.noreply.github.com>
2023-02-05 14:55:01 +0300

    Changed imports.

--- integration-tests/test_redis_to_relational.py
+++ integration-tests/test_redis_to_relational.py
@@ -1,19 +1,5 @@
 import logging
 
-try:
-    # This hack makes it possible to use the new driver with SQLAlchemy 1.4.*
-    # More: https://lnk.pw/swif
-
-    import sys
-
-    import oracledb
-
-    oracledb.version = "8.3.0"
-    sys.modules["cx_Oracle"] = oracledb
-    oracledb.init_oracle_client()
-except ImportError:
-    pass
-
 import pytest
 from common import db_utils, redis_utils
 from common.utils import run_job
