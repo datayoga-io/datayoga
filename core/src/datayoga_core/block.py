e19dccbe08a2f713b4ee247bb6760323e3e11ed4
vd2org <33198864+vd2org@users.noreply.github.com>
2023-02-08 17:00:09 +0300

    Fixed import.

--- core/src/datayoga_core/block.py
+++ core/src/datayoga_core/block.py
@@ -95,6 +95,9 @@ class Block(metaclass=ABCMeta):
 
     @staticmethod
     def create(block_name: str, properties: Dict[str, Any]) -> Block:
+        # Fixes oracle hack import.
+        import datayoga_core.blocks.relational # noqa
+
         module_name = f"datayoga_core.blocks.{block_name}.block"
         module = importlib.import_module(module_name)
         block: Block = getattr(module, "Block")(properties)
