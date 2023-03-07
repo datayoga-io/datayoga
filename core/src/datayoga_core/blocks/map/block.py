ab6ee1df33a5d1af866674ac8f93f0f0ad02139e
vd2org <33198864+vd2org@users.noreply.github.com>
2023-03-07 19:11:36 +0300

    Nested fields mapping.

--- core/src/datayoga_core/blocks/map/block.py
+++ core/src/datayoga_core/blocks/map/block.py
@@ -1,11 +1,11 @@
-import json
 import logging
 from abc import ABCMeta
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 from datayoga_core import expression
 from datayoga_core.block import Block as DyBlock
 from datayoga_core.context import Context
+from datayoga_core.expression import Language
 from datayoga_core.result import BlockResult, Result, Status
 
 logger = logging.getLogger("dy")
@@ -13,25 +13,36 @@ logger = logging.getLogger("dy")
 
 class Block(DyBlock, metaclass=ABCMeta):
 
+    @staticmethod
+    def _prepare_expression(language: Language, expr: Union[dict, str]) -> str:
+        if isinstance(expr, dict):
+            expr = ", ".join([f"{k}: {v}" for k, v in expr.items()])
+            expr = f"{{{expr}}}"
+
+        expr = expr.strip()
+
+        if language == Language.JMESPATH:
+            # jmespath expression for map block must be enclosed in { }
+            if not (expr.startswith("{") and expr.endswith("}")):
+                raise ValueError("map expression must be in a json-like format enclosed in { }")
+
+        return expr
+
     def init(self, context: Optional[Context] = None):
         logger.debug(f"Initializing {self.get_block_name()}")
-        expression_prop = json.dumps(
-            self.properties["expression"]) if isinstance(
-            self.properties["expression"],
-            dict) else self.properties["expression"]
-        # the expression is now a string (jmespath expressions are not valid JSON)
-        expression_prop = expression_prop.strip()
-        if not (expression_prop.startswith("{") and expression_prop.endswith("}")):
-            raise ValueError("map expression must be in a json-like format enclosed in { }")
-        self.expression = expression.compile(self.properties["language"], expression_prop)
+
+        language = self.properties["language"]
+        expression_prop = self._prepare_expression(language, self.properties["expression"])
+
+        self.expression = expression.compile(language, expression_prop)
 
     async def run(self, data: List[Dict[str, Any]]) -> BlockResult:
         logger.debug(f"Running {self.get_block_name()}")
         mapped_rows = self.expression.search_bulk(data)
         # we always add the internal fields back, in case they were dropped in the map operation
         for mapped_row, original_row in zip(mapped_rows, data):
-            original_msg_id= original_row.get(Block.MSG_ID_FIELD)
-            original_opcode= original_row.get(Block.OPCODE_FIELD)
+            original_msg_id = original_row.get(Block.MSG_ID_FIELD)
+            original_opcode = original_row.get(Block.OPCODE_FIELD)
             if original_msg_id is not None:
                 mapped_row[Block.MSG_ID_FIELD] = original_msg_id
             if original_opcode is not None:
