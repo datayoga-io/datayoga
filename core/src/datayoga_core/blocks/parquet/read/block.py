79317ba2ebe3e6e7d3638c0dacd69a84c96d9ba4
vd2org <33198864+vd2org@users.noreply.github.com>
2023-03-08 15:12:31 +0300

    Implemented parquet reader.

---
+++ core/src/datayoga_core/blocks/parquet/read/block.py
@@ -0,0 +1,39 @@
+import json
+import logging
+import os
+from abc import ABCMeta
+from typing import Generator, Optional
+
+from datayoga_core.context import Context
+from datayoga_core.producer import Message
+from datayoga_core.producer import Producer as DyProducer
+from fastparquet import ParquetFile
+
+logger = logging.getLogger("dy")
+
+
+class Block(DyProducer, metaclass=ABCMeta):
+
+    def init(self, context: Optional[Context] = None):
+        logger.debug(f"Initializing {self.get_block_name()}")
+        parquet_file = self.properties["file"]
+
+        if os.path.isabs(parquet_file) or context is None:
+            self.file = parquet_file
+        else:
+            self.file = os.path.join(context.properties.get("data_path"), parquet_file)
+
+        logger.debug(f"file: {self.file}")
+        self.batch_size = self.properties.get("batch_size", 1000)
+
+    def produce(self) -> Generator[Message, None, None]:
+        logger.debug("Reading parquet")
+
+        pf = ParquetFile(self.file)
+
+        count = 0
+        for df in pf.iter_row_groups():
+            for _, data in df.iterrows():
+                data = json.loads(data.to_dict()['data'])
+                yield {self.MSG_ID_FIELD: str(count), **data}
+                count += 1
