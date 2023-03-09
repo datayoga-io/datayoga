1382119ee7c1f014d6e40c3da000509ba48a9d44
vd2org <33198864+vd2org@users.noreply.github.com>
2023-03-09 15:29:59 +0300

    Implemented parquet.write block.

---
+++ core/src/datayoga_core/blocks/parquet/write/block.py
@@ -0,0 +1,41 @@
+
+import logging
+import os
+from abc import ABCMeta
+from typing import Any, Dict, List, Optional
+
+from datayoga_core import utils
+from datayoga_core.block import Block as DyBlock
+from datayoga_core.context import Context
+from datayoga_core.result import BlockResult
+from fastparquet import write
+from pandas import DataFrame
+
+logger = logging.getLogger("dy")
+
+
+class Block(DyBlock, metaclass=ABCMeta):
+
+    def init(self, context: Optional[Context] = None):
+        logger.debug(f"Initializing {self.get_block_name()}")
+        parquet_file = self.properties["file"]
+
+        if os.path.isabs(parquet_file) or context is None:
+            self.file = parquet_file
+        else:
+            self.file = os.path.join(context.properties.get("data_path"), parquet_file)
+
+        logger.debug(f"file: {self.file}")
+        self.batch_size = self.properties.get("batch_size", 1000)
+
+    async def run(self, data: List[Dict[str, Any]]) -> BlockResult:
+        logger.debug("Writing parquet")
+
+        # remove the internal $$msg_id column
+        out = [dict(data={i: record[i] for i in record if i != Block.MSG_ID_FIELD}) for record in data]
+
+        append = os.path.exists(self.file)
+        write(self.file, DataFrame(out), append=append, has_nulls=True)
+
+        # if we made it here, it is a success. return the data and the success result
+        return utils.all_success(data)
