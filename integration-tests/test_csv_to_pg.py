d724129adb3ef04e82f5b28ad9da45e9d6421b52
vd2org <33198864+vd2org@users.noreply.github.com>
2023-04-11 15:34:41 +0300

    Implemented test.

--- integration-tests/test_csv_to_pg.py
+++ integration-tests/test_csv_to_pg.py
@@ -1,5 +1,7 @@
 import logging
+from contextlib import suppress
 
+import pytest.mark
 from common import db_utils
 from common.utils import run_job
 
@@ -8,38 +10,49 @@ logger = logging.getLogger("dy")
 SCHEMA = "hr"
 
 
-def test_csv_to_pg():
-    postgres_container = db_utils.get_postgres_container("postgres", "postgres", "postgres")
-    postgres_container.start()
+@pytest.mark.parametrize(
+    "job_name",
+    [
+        # normal test
+        ("tests.csv_to_pg",),
+        # test with invalid data in the batch
+        ("tests.csv_to_pg_batch_with_invalid",),
+    ]
+)
+def test_csv_to_pg(job_name: str):
+    try:
+        postgres_container = db_utils.get_postgres_container("postgres", "postgres", "postgres")
+        postgres_container.start()
 
-    engine = db_utils.get_engine(postgres_container)
-    db_utils.create_schema(engine, SCHEMA)
-    db_utils.create_emp_table(engine, SCHEMA)
+        engine = db_utils.get_engine(postgres_container)
+        db_utils.create_schema(engine, SCHEMA)
+        db_utils.create_emp_table(engine, SCHEMA)
 
-    run_job("tests.csv_to_pg")
+        run_job(job_name)
 
-    total_employees = db_utils.select_one_row(engine, f"select count(*) as total from {SCHEMA}.emp")
-    assert total_employees and total_employees["total"] == 3
+        total_employees = db_utils.select_one_row(engine, f"select count(*) as total from {SCHEMA}.emp")
+        assert total_employees and total_employees["total"] == 3
 
-    first_employee = db_utils.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 1")
-    assert first_employee is not None
-    assert first_employee["id"] == 1
-    assert first_employee["full_name"] == "John Doe"
-    assert first_employee["country"] == "972 - ISRAEL"
-    assert first_employee["gender"] == "M"
+        first_employee = db_utils.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 1")
+        assert first_employee is not None
+        assert first_employee["id"] == 1
+        assert first_employee["full_name"] == "John Doe"
+        assert first_employee["country"] == "972 - ISRAEL"
+        assert first_employee["gender"] == "M"
 
-    second_employee = db_utils.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 2")
-    assert second_employee is not None
-    assert second_employee["id"] == 2
-    assert second_employee["full_name"] == "Jane Doe"
-    assert second_employee["country"] == "972 - ISRAEL"
-    assert second_employee["gender"] == "F"
+        second_employee = db_utils.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 2")
+        assert second_employee is not None
+        assert second_employee["id"] == 2
+        assert second_employee["full_name"] == "Jane Doe"
+        assert second_employee["country"] == "972 - ISRAEL"
+        assert second_employee["gender"] == "F"
 
-    third_employee = db_utils.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 3")
-    assert third_employee is not None
-    assert third_employee["id"] == 3
-    assert third_employee["full_name"] == "Bill Adams"
-    assert third_employee["country"] == "1 - USA"
-    assert third_employee["gender"] == "M"
-
-    postgres_container.stop()
+        third_employee = db_utils.select_one_row(engine, f"select * from {SCHEMA}.emp where id = 3")
+        assert third_employee is not None
+        assert third_employee["id"] == 3
+        assert third_employee["full_name"] == "Bill Adams"
+        assert third_employee["country"] == "1 - USA"
+        assert third_employee["gender"] == "M"
+    finally:
+        with suppress(Exception):
+            postgres_container.stop()  # noqa
