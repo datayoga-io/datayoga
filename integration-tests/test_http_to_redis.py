860d4c9dec7dbca6e3e404757a7a86dc7b70dc91
Ivan Kolesnikov <33198864+vd2org@users.noreply.github.com>
2023-05-22 15:42:14 +0000

    Fixed.

--- integration-tests/test_http_to_redis.py
+++ integration-tests/test_http_to_redis.py
@@ -17,6 +17,7 @@ def test_http_to_redis():
 
     try:
         redis_container.start()
+        run_job("tests.http_to_redis")
         program = run_job("tests.http_to_redis", background=True)
 
         for i in range(30):
