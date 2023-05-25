197907deb10906124afee9c6d3807edf9758d644
vd2org <33198864+vd2org@users.noreply.github.com>
2023-05-25 16:05:47 +0300

    Fixed.

--- core/src/datayoga_core/prom.py
+++ core/src/datayoga_core/prom.py
@@ -2,7 +2,7 @@ from prometheus_client import Counter, start_http_server
 
 incoming_records = Counter('incoming_records', 'Number of incoming records')
 
-processed_entries = Counter('processed_entries', 'Number of processed records', ('step',))
+processed_entries = Counter('processed_records', 'Number of processed records', ('step',))
 rejected_records = Counter('rejected_records', 'Number of rejected records', ('step',))
 filtered_records = Counter('filtered_records', 'Number of filtered records', ('step',))
 
