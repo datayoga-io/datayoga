55c6be17decbe7fc29047926abe79316aafd7cc0
vd2org <33198864+vd2org@users.noreply.github.com>
2023-05-30 15:02:03 +0300

    Reformat.

--- core/src/datayoga_core/prometheus.py
+++ core/src/datayoga_core/prometheus.py
@@ -1,10 +1,9 @@
 from prometheus_client import Counter, start_http_server
 
-incoming_records = Counter('incoming_records', 'Number of incoming records')
-
-processed_entries = Counter('processed_records', 'Number of processed records', ('step',))
-rejected_records = Counter('rejected_records', 'Number of rejected records', ('step',))
-filtered_records = Counter('filtered_records', 'Number of filtered records', ('step',))
+incoming_records = Counter("incoming_records", "Number of incoming records")
+processed_entries = Counter("processed_records", "Number of processed records", ("step",))
+rejected_records = Counter("rejected_records", "Number of rejected records", ("step",))
+filtered_records = Counter("filtered_records", "Number of filtered records", ("step",))
 
 
 def start(port: int):
