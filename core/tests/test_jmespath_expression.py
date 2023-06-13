564abdb188a40e9ff48822893e32f3b384b903df
vd2org <33198864+vd2org@users.noreply.github.com>
2023-06-13 19:33:33 +0300

    Implemented.

--- core/tests/test_jmespath_expression.py
+++ core/tests/test_jmespath_expression.py
@@ -246,3 +246,8 @@ def test_jmespath_filter_tombstone():
 def test_jmespath_json_parse():
     expression.compile("json_parse(data)")
     assert expression.search({"data": """{"greeting": "hello world!"}"""}) == {"greeting": "hello world!"}
+
+
+def test_jmespath_base64_decode():
+    expression.compile("base64_decode(data)")
+    assert expression.search({"data": "SGVsbG8gV29ybGQh"}) == "Hello World!"
