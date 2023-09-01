cd762e3332b6f85444d2e2df395fb5d36d9a0fa4
vd2org <33198864+vd2org@users.noreply.github.com>
2023-09-01 23:24:47 +0300

    Fixed.

--- integration-tests/test_redis_lookup.py
+++ integration-tests/test_redis_lookup.py
@@ -10,9 +10,9 @@ REDIS_PORT = 12554
     [("tests.redis_lookup_string", "0", "None"),
      ("tests.redis_lookup_string", "1", "test_string"),
      ("tests.redis_lookup_hash", "2", "{'tf0': 'tv0', 'tf1': 'tv1'}"),
-     ("tests.redis_lookup_hash", "3", "{'tf0': 'tv0', 'tf1': 'tv1'}"),
-     ("tests.redis_lookup_hash", "4", "['tv0', '10', 'tv1', '20']"),
-     ("tests.redis_lookup_hash", "5", "['tv2', 'tv1', 'tv0']")])
+     ("tests.redis_lookup_set", "3", "{'tf0': 'tv0', 'tf1': 'tv1'}"),
+     ("tests.redis_lookup_sorted_set", "4", "['tv0', '10', 'tv1', '20']"),
+     ("tests.redis_lookup_list", "5", "['tv2', 'tv1', 'tv0']")])
 def test_redis_lookup(configuration, key, expected):
     redis_container = redis_utils.get_redis_oss_container(REDIS_PORT)
     redis_container.start()
