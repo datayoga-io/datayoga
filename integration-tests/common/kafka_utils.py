9a741933dc692bb560bdfff421531d8fd4798493
wheelly <borisk@centerity-ext.com>
2024-02-18 11:26:55 +0200

    #138 - kafka.read - reformat - missed file

--- integration-tests/common/kafka_utils.py
+++ integration-tests/common/kafka_utils.py
@@ -1,8 +1,11 @@
 from kafka import KafkaProducer
 from testcontainers.kafka import KafkaContainer
+
+
 def get_kafka_container() -> KafkaContainer:
     return (KafkaContainer(image="confluentinc/cp-kafka:latest")
             .with_bind_ports(KafkaContainer.KAFKA_PORT, KafkaContainer.KAFKA_PORT))
+
 
 def get_kafka_producer(bootstrap_servers: str) -> KafkaProducer:
     return KafkaProducer(bootstrap_servers=bootstrap_servers)
